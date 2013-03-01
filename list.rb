c59fd26bd65e525dab12a010dea81081de923176
Apprentice <apprentice@dbc27.local>
2013-03-01 14:23:24 -0800

    updates to task and list classes and list method

--- list.rb
+++ list.rb
@@ -9,29 +9,38 @@ class List
     counter = 1
     case list_type.downcase
       when 'all' then @master_list.each do |task|
-        puts "#{counter}. #{task.body}"
+        puts "#{counter}. #{task.body} - #{task.status.capitalize}"
         counter += 1
       end
       when 'completed' then @completed_list.each do |task|
-        puts "#{counter}. #{task.body}"
+        puts "#{counter}. #{task.body} - Completed"
         counter += 1
       end
       when 'open' then @open_list.each do |task|
-        puts "#{counter}. #{task.body}"
+        puts "#{counter}. #{task.body} - Open"
         counter += 1
       end
     end  
   end
 
   def add
+    @master_list << Task.new(body)
+    bucket
+  end
+
+  def delete(num)
     
   end
 
-  def delete
-    
+  def complete(num)
+    if list_type ==  
+    bucket
   end
 
-  def complete
-    
-  end
+  def bucket
+    @master_list.each do |task|
+      @completed_list << task if task.status == :completed
+      @open_list << task if task.status == :open
+    end  
+  end    
 end
